a954dbb92d29c5afd7a70283625c6cb4878292f6
asasas333_3 <am.mesbahi@gmail.com>
2022-02-18 14:00:33 +0100

    format file

--- agent/openvas.py
+++ agent/openvas.py
@@ -1,3 +1,4 @@
+"""Wrapper for OpenVas scanner to start the scan and extract the results."""
 import datetime
 import logging
 
@@ -15,9 +16,13 @@ GMP_PASSWORD = 'admin'
 
 
 class OpenVas:
-
+    """OpenVas wrapper to enable using openvas scanner from ostorlab agent class."""
     def start_scan(self, ip):
+        """Start OpenVas scan on the ip provided.
 
+        Args:
+            ip: Target ip to scan.
+        """
         connection = gvm.connections.TLSConnection(hostname='localhost')
         transform = transforms.EtreeTransform()
         with openvas_gmp.Gmp(connection, transform=transform) as gmp:
@@ -27,16 +32,36 @@ class OpenVas:
             report_id = self._start_task(gmp, task_id)
             logger.info('Started scan of host %s. Corresponding report ID is %s', str(ip), str(report_id))
 
-    def _create_target(self, gmp, ip_address, port_list_id):
-        name = f'Testing Host {ip_address} {datetime.datetime.now()}'
-        response = gmp.create_target(name=name, hosts=[ip_address], port_list_id=port_list_id)
+    def _create_target(self, gmp, ip, port_list_id):
+        """Create gmp target https://docs.greenbone.net/API/GMP/gmp-21.04.html#command_create_target.
+
+        Args:
+            gmp: GMP object.
+            ip: Target ip to scan.
+            port_list_id: ports to scan
+
+        Returns:
+            - target id.
+        """
+        name = f'Testing Host {ip} {datetime.datetime.now()}'
+        response = gmp.create_target(name=name, hosts=[ip], port_list_id=port_list_id)
+        return response.get('id')
+
+    def _create_task(self, gmp, ip, target_id, scan_config_id, scanner_id):
+        """Create gmp task https://docs.greenbone.net/API/GMP/gmp-21.04.html#command_create_task.
+
+        Args:
+            gmp: GMP object.
+            ip: Target ip to scan.
+            port_list_id: ports to scan
+
+        Returns:
+            - target id.
+        """
+        name = f'Scan Host {ip_address}'
+        response = gmp.create_task(name=name, config_id=scan_config_id, target_id=target_id, scanner_id=scanner_id,)
         return response.get('id')
 
     def _start_task(self, gmp, task_id):
         response = gmp.start_task(task_id)
         return response[0].text
-
-    def _create_task(self, gmp, ip_address, target_id, scan_config_id, scanner_id):
-        name = f'Scan Host {ip_address}'
-        response = gmp.create_task(name=name, config_id=scan_config_id, target_id=target_id, scanner_id=scanner_id,)
-        return response.get('id')
