0dfc3687a5c7c7a485491284df715c9f170a88e8
chevalier3as <chevalier3as@gmail.com>
2022-02-18 11:34:03 +0100

    Prepare dockerfile and cleanup sample code.

--- tests/create_scan.py
+++ tests/create_scan.py
@@ -12,19 +12,11 @@ fire up the program python vulscan.py A. it scan localhost for the demo. It will
 import gvm
 from gvm.protocols.gmp import Gmp
 from gvm.transforms import EtreeTransform
-from gvm.xml import pretty_print
-import xml.etree.ElementTree as ET
-import untangle
-import base64
-import csv, json
 import datetime
-import sys
-
 
 def create_target(gmp, ipaddress, port_list_id):
     # create a unique name by adding the current datetime
     name = "Suspect Host {} {}".format(ipaddress, str(datetime.datetime.now()))
-
     response = gmp.create_target(
         name=name, hosts=[ipaddress], port_list_id=port_list_id
     )
@@ -50,7 +42,7 @@ def create_task(gmp, ipaddress, target_id, scan_config_id, scanner_id):
 
 
 def main():
-    ipaddress = "localhost"
+    ipaddress = "8.8.8.8"
     # ex. iana-tcp-udp scan
     port_list_id = "4a4717fe-57d2-11e1-9a26-406186ea4fc5"
     connection = gvm.connections.TLSConnection(hostname='localhost')
@@ -58,10 +50,8 @@ def main():
     with Gmp(connection, transform=transform) as gmp:
         gmp.authenticate('admin', 'admin')
         target_id = create_target(gmp, ipaddress, port_list_id)
-
         full_and_fast_scan_config_id = 'daba56c8-73ec-11df-a475-002264764cea'
         openvas_scanner_id = '08b69003-5fc2-4037-a479-93b440211c73'
-
         task_id = create_task(
             gmp,
             ipaddress,
@@ -69,9 +59,7 @@ def main():
             full_and_fast_scan_config_id,
             openvas_scanner_id,
         )
-
         report_id = start_task(gmp, task_id)
-
         print(
             "Started scan of host {}. Corresponding report ID is {}".format(
                 ipaddress, report_id
--- tests/get_report.py
+++ tests/get_report.py
@@ -1,6 +1,5 @@
-from flask import Flask, json, request
+from flask import Flask, request
 from flask_pymongo import PyMongo
-import requests, json
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/openvasdb"
@@ -11,9 +10,7 @@ mongo = PyMongo(app)
 def get_reprots():
     if request.method == 'POST':
         reportdate = request.form.get('selectedDate')
-
     reports = mongo.db.vul_reports.find({"report_date": reportdate}).pretty()
-
     return flask.jsonify([report for report in reports])
 
 
