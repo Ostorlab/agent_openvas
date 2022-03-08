87af107f8bc0bf44d56f335786ab472ed3950a7b
chevalier3as <chevalier3as@gmail.com>
2022-03-08 16:05:56 +0100

    Fix unit test.

--- tests/openvas_agent_test.py
+++ tests/openvas_agent_test.py
@@ -2,12 +2,13 @@
 import json
 
 from ostorlab.agent import definitions as agent_definitions
-from ostorlab.runtimes import definitions as runtime_definitions
-from ostorlab.utils import defintions as utils_definitions
 from ostorlab.agent.kb import kb
 from ostorlab.agent.mixins import agent_report_vulnerability_mixin
+from ostorlab.runtimes import definitions as runtime_definitions
+from ostorlab.utils import defintions as utils_definitions
 
 from agent import openvas_agent
+
 
 def testAgentOpenVas_whenBinaryAvailable_RunScan(scan_message, mocker):
     """Tests running the agent and parsing the json output."""
@@ -22,10 +23,10 @@ def testAgentOpenVas_whenBinaryAvailable_RunScan(scan_message, mocker):
             utils_definitions.Arg(name='reporting_engine_base_url', type='str', value=b'https://toto.ostorlab.co/test'),
             utils_definitions.Arg(name='reporting_engine_token', type='str', value=b'123456')],
         healthcheck_port=5301)
-    mocker.patch('agent.openvas.OpenVas.start_scan',return_value='hduzehfuhehfuhef')
-    mocker.patch('agent.openvas.OpenVas.wait_task',return_value=None)
+    mocker.patch('agent.openvas.OpenVas.start_scan', return_value='hduzehfuhehfuhef')
+    mocker.patch('agent.openvas.OpenVas.wait_task', return_value=None)
     with open('tests/openvas_result.csv', 'r', encoding='UTF-8') as f:
-        mocker.patch('agent.openvas.OpenVas.get_results',return_value=f.read())
+        mocker.patch('agent.openvas.OpenVas.get_results', return_value=f.read())
         mock_report_vulnerability = mocker.patch('agent.openvas_agent.OpenVasAgent.report_vulnerability',
                                                  return_value=None)
         test_agent = openvas_agent.OpenVasAgent(definition, settings)
@@ -38,15 +39,15 @@ def testAgentOpenVas_whenBinaryAvailable_RunScan(scan_message, mocker):
                   'Affected Software/OS': '', 'Vulnerability Insight': '', 'Vulnerability Detection Method': '',
                   'Product Detection Result': '', 'BIDs': '', 'CERTs': '', 'Other References': ''}
 
-        mock_report_vulnerability.assert_called_with(entry= kb.Entry(title='openvas', risk_rating='INFO',
-                                                                     references={}, short_description='',
-                                                                     description='', recommendation='',
-                                                                     security_issue=True, privacy_issue=False,
-                                                                     has_public_exploit=False,
-                                                                     targeted_by_malware=False,
-                                                                     targeted_by_ransomware=False,
-                                                                     targeted_by_nation_state=False,
-                                                                     cvss_v3_vector=''),
-                                                     risk_rating= agent_report_vulnerability_mixin.RiskRating.INFO,
+        mock_report_vulnerability.assert_called_with(entry=kb.Entry(title='', risk_rating='INFO',
+                                                                    references={}, short_description='',
+                                                                    description='', recommendation='',
+                                                                    security_issue=True, privacy_issue=False,
+                                                                    has_public_exploit=False,
+                                                                    targeted_by_malware=False,
+                                                                    targeted_by_ransomware=False,
+                                                                    targeted_by_nation_state=False,
+                                                                    cvss_v3_vector=''),
+                                                     risk_rating=agent_report_vulnerability_mixin.RiskRating.INFO,
                                                      technical_detail=
-                                                     f'```json\n{json.dumps(output, indent=4, sort_keys=True)}\n```')
+                                                     f'\n```json\n{json.dumps(output, indent=4, sort_keys=True)}\n```')
