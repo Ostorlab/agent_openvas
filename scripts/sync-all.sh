80ca7ce400bd70a4c005cff99b9afbca158ab64b
amine <amine.mesbahi@ostorlab.dev>
2022-08-27 00:32:50 +0200

    Remove unused files

--- scripts/sync-all.sh
+++ scripts/sync-all.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Updating NVTs..."
-mkdir -p /usr/local/var/run/
-chown -R openvas-sync:openvas-sync  /usr/local/var/run/
-su -c "greenbone-nvt-sync" openvas-sync
 su -c "rsync --compress-level=9 --links --times --omit-dir-times --recursive --partial --quiet rsync://feed.community.greenbone.net:/nvt-feed /usr/local/var/lib/openvas/plugins" openvas-sync
 #sleep 5
 
