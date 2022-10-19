a38f1f6dcff592ee5d8d55260ba176407dbc28c7
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-10-19 14:26:57 +0100

    Add targets dataclasses definition s.

---
+++ agent/targetables.py
@@ -0,0 +1,19 @@
+"""Definitions of targetable assets by the openvas agent."""
+import dataclasses
+from typing import Optional
+
+
+@dataclasses.dataclass
+class DomainTarget:
+    """Domain name target dataclass definition."""
+    name: str
+
+
+@dataclasses.dataclass
+class IPTarget:
+    """IP address target dataclass definition."""
+    name: str
+    version: int
+    mask: Optional[str] = None
+    port: Optional[str] = None
+    schema: Optional[str] = None
