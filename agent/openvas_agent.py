ce92609bc5267b52136036e6782be9f3d1301bef
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-10-19 14:26:24 +0100

    Refactor code and add vulnerable target.

--- agent/openvas_agent.py
+++ agent/openvas_agent.py
@@ -6,7 +6,7 @@ import subprocess
 import time
 from urllib import parse
 import ipaddress
-from typing import Optional
+from typing import Dict, Optional
 import re
 
 from ostorlab.agent import agent
@@ -16,9 +16,14 @@ from ostorlab.agent.mixins import agent_report_vulnerability_mixin
 from ostorlab.agent.mixins import agent_persist_mixin as persist_mixin
 from ostorlab.agent import definitions as agent_definitions
 from ostorlab.runtimes import definitions as runtime_definitions
+from ostorlab.assets import domain_name as domain_asset
+from ostorlab.assets import ipv4 as ipv4_asset
+from ostorlab.assets import ipv6 as ipv6_asset
 from rich import logging as rich_logging
 
 from agent import openvas
+from agent import targetables
+
 
 logging.basicConfig(
     format='%(message)s',
@@ -76,39 +81,40 @@ class OpenVasAgent(agent.Agent, agent_report_vulnerability_mixin.AgentReportVuln
         target = None
 
         if message.data.get('name') is not None:
-            target = self._prepare_target_url(message)
-            if not self.set_add(STORAGE_NAME, target):
-                logger.info('target %s was processed before, exiting', target)
+            target = self._prepare_target_from_domain_msg(message)
+            if not self.set_add(STORAGE_NAME, target.name):
+                logger.info('target %s was processed before, exiting', target.name)
                 return
 
         elif message.data.get('url') is not None:
-            target = self._prepare_target_name(message)
-            if not self.set_add(STORAGE_NAME, target):
-                logger.info('target %s was processed before, exiting', target)
+            target = self._prepare_target_from_link_msg(message)
+            if not self.set_add(STORAGE_NAME, target.name):
+                logger.info('target %s was processed before, exiting', target.name)
                 return
 
         elif message.data.get('host') is not None:
-            target = self._prepare_target_host(message)
-            addresses = ipaddress.ip_network(target, strict=False)
+            target = self._prepare_target_from_ip_msg(message)
+            addresses = ipaddress.ip_network(target.name, strict=False)
             if not self.add_ip_network(STORAGE_NAME, addresses):
-                logger.info('target %s was processed before, exiting', target)
+                logger.info('target %s was processed before, exiting', target.name)
                 return
-
-        if target is None:
-            return
         else:
-            logger.info('scanning target %s', target)
-            if not self._should_process_target(self._scope_regex, target):
-                return
-            task_id = openvas_wrapper.start_scan(target,
-                                                 self.args.get('scan_config_id', openvas.GVMD_FULL_FAST_CONFIG)
-                                                 )
-            openvas_wrapper.wait_task(task_id)
-            result = openvas_wrapper.get_results()
-            if result is not None:
-                self._persist_results(result)
-                self._process_results()
-            logger.info('Scan finished.')
+            logger.info('Target not provided')
+            return
+
+        url_in_scope = self._is_url_in_scan_scope(target.name, self._scope_regex)
+        if isinstance(target, targetables.DomainTarget) and url_in_scope is False:
+            return
+
+        logger.info('scanning target %s', target.name)
+        task_id = openvas_wrapper.start_scan(target.name,
+                                             self.args.get('scan_config_id', openvas.GVMD_FULL_FAST_CONFIG))
+        openvas_wrapper.wait_task(task_id)
+        result = openvas_wrapper.get_results()
+        if result is not None:
+            self._persist_results(result)
+            self._process_results(target)
+        logger.info('Scan finished.')
 
     def _wait_vt_ready(self):
         """when started, Openvas first loads all the VT to the database
@@ -130,7 +136,34 @@ class OpenVasAgent(agent.Agent, agent_report_vulnerability_mixin.AgentReportVuln
         with open(CSV_PATH_OUTPUT, 'w', encoding='UTF-8') as f:
             f.write(results)
 
-    def _process_results(self):
+    def _get_vulnerable_target_data(self,
+                                    target: targetables.DomainTarget | targetables.IPTarget,
+                                    vuln: Dict[str, str]
+                                    ) -> Optional[agent_report_vulnerability_mixin.VulnerabilityLocation]:
+        """Returns the exact target where the vulnerability was detected,
+        eg: In which IP of the given range the vulnerability was found."""
+        metadata = []
+        if vuln.get('Port', '') != '':
+            metadata_type = agent_report_vulnerability_mixin.MetadataType.PORT
+            metadata_value = vuln.get('Port')
+            metadata = [
+                agent_report_vulnerability_mixin.VulnerabilityLocationMetadata(type=metadata_type, value=metadata_value)
+            ]
+        if isinstance(target, targetables.DomainTarget) and vuln.get('Hostname', '') != '':
+            asset = domain_asset.DomainName(name=vuln.get('Hostname'))
+            return agent_report_vulnerability_mixin.VulnerabilityLocation(asset=asset, metadata=metadata)
+        elif isinstance(target, targetables.IPTarget) and vuln.get('IP', '') != '':
+            vulnerable_host = vuln.get('IP')
+            if target.version==4:
+                asset = ipv4_asset.IPv4(host = vulnerable_host, version = 4, mask = '32')
+            else:
+                asset = ipv6_asset.IPv6(host = vulnerable_host, version = 6, mask = '128')
+            return agent_report_vulnerability_mixin.VulnerabilityLocation(asset=asset, metadata=metadata)
+        else:
+            raise NotImplementedError(f'type target {type(target)} not implemented')
+
+
+    def _process_results(self, target: targetables.DomainTarget | targetables.IPTarget):
         """read and parse the output file and send the findings"""
 
         with open(CSV_PATH_OUTPUT, encoding='UTF-8') as csv_file:
@@ -139,6 +172,7 @@ class OpenVasAgent(agent.Agent, agent_report_vulnerability_mixin.AgentReportVuln
                 detail = line_result.get('Specific Result', '')
                 detail += '\n'
                 detail += f'```json\n{json.dumps(line_result, indent=4, sort_keys=True)}\n```'
+                vulnerability_location = self._get_vulnerable_target_data(target, line_result)
                 self.report_vulnerability(
                     entry=kb.Entry(
                         title=line_result.get('NVT Name', 'OpenVas Finding'),
@@ -156,33 +190,36 @@ class OpenVasAgent(agent.Agent, agent_report_vulnerability_mixin.AgentReportVuln
                         targeted_by_nation_state=False
                     ),
                     technical_detail=detail,
-                    risk_rating=_severity_map(line_result.get('severity', 'INFO').lower()))
+                    risk_rating=_severity_map(line_result.get('severity', 'INFO').lower()),
+                    vulnerability_location=vulnerability_location)
 
-    def _should_process_target(self, scope_regex: Optional[str], url: str) -> bool:
+
+    def _is_url_in_scan_scope(self, url: str, scope_regex: Optional[str] = None) -> bool:
         if scope_regex is None:
             return True
-        link_in_scan_domain = re.match(scope_regex, url) is not None
-        if not link_in_scan_domain:
+        if re.match(scope_regex, url) is None:
             logger.warning('link url %s is not in domain %s', url, scope_regex)
-        return link_in_scan_domain
+            return False
+        else:
+            return True
 
-    def _prepare_target_url(self, message: m.Message) -> str:
-        target = message.data.get('name')
-        return target
+    def _prepare_target_from_domain_msg(self, message: m.Message) -> targetables.DomainTarget:
+        domain_name = message.data.get('name', '')
+        return targetables.DomainTarget(name=domain_name)
 
-    def _prepare_target_name(self, message: m.Message) -> str:
-        target = parse.urlparse(message.data.get('url')).netloc
-        return target
+    def _prepare_target_from_link_msg(self, message: m.Message) -> targetables.DomainTarget:
+        domain_name = parse.urlparse(message.data.get('url')).netloc
+        return targetables.DomainTarget(name=domain_name)
 
-    def _prepare_target_host(self, message: m.Message) -> str:
+    def _prepare_target_from_ip_msg(self, message: m.Message) -> targetables.IPTarget:
         host = message.data.get('host')
         version = ipaddress.ip_address(host).version
         default_mask = '32' if version == 4 else '128'
         mask = message.data.get('mask', default_mask)
         if mask == default_mask:
-            target = host
+            target = targetables.IPTarget(name=host, version=version)
         else:
-            target = f'{host}/{mask}'
+            target = targetables.IPTarget(name=f'{host}/{mask}', version=version, mask=mask)
         return target
 
 
